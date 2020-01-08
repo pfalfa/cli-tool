028e8ac5771befc74c073cb834b3f4971d35db1d
eksant <eksant@gmail.com>
2020-01-08 17:54:21 +0800

    Update CLI list

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,10 +1,40 @@
-const {Command, flags} = require('@oclif/command')
+const moment = require('moment')
+const Table = require('cli-table')
+const { Command } = require('@oclif/command')
+const { api, msg, db } = require('../utils')
 
 class ListCommand extends Command {
   async run() {
-    const {flags} = this.parse(ListCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /Users/adisetiawan/project/pfalfa-cli-tool/src/commands/list.js`)
+    const user = await db.read()
+    if (user === undefined) return this.log(msg.error('Please login first!'))
+
+    api
+      .get(api.host.pfalfa, 'dapps', user.pubkey)
+      .then(resp => {
+        const { success, message, data } = resp
+        if (!success) return this.log(msg.fail(message))
+
+        const table = new Table({
+          head: ['DApp Name', 'Category', 'IP Public', 'Port', 'Gun DB', 'Status', 'Created At'],
+        })
+        data.length > 0 &&
+          data.forEach(i => {
+            table.push([
+              i.name || '',
+              i.category || '',
+              i.ipPublic || '',
+              i.port || '',
+              i.gunDb || '',
+              i.status || '',
+              (i.createdAt && moment(i.createdAt).format('DD MMM YYYY hh:mm:ss')) || '',
+            ])
+          })
+
+        return this.log(table.toString())
+      })
+      .catch(error => {
+        return this.log(msg.error(error))
+      })
   }
 }
 
@@ -12,7 +42,5 @@ ListCommand.description = `List existing dApps
 ...
 Extra documentation goes here
 `
-
-
 
 module.exports = ListCommand
